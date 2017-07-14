d06082dec415ee93b860a0e4c5815b171804c329
Bill Taggart <wt49@njit.edu>
2017-07-14 18:13:25 -0400

    updated index.php

--- index.php
+++ index.php
@@ -14,31 +14,72 @@ switch ($action) {
         $email = filter_input(INPUT_POST, 'email');
         $phone = filter_input(INPUT_POST, 'phone');
 
-        /*************************************************
-         * validate and process the name
-         ************************************************/
-        // 1. make sure the user enters a name
-        // 2. display the name with only the first letter capitalized
+        //  make sure the user enters a name
+	if (empty($name)) {
+           $message = 'Name is missing!';
+           break;
+        }
 
-        /*************************************************
-         * validate and process the email address
-         ************************************************/
-        // 1. make sure the user enters an email
-        // 2. make sure the email address has at least one @ sign and one dot character
+        //  display the name with only the first letter capitalized
+        $name = strtolower($name);
+        $name = ucwords($name);
 
-        /*************************************************
-         * validate and process the phone number
-         ************************************************/
-        // 1. make sure the user enters at least seven digits, not including formatting characters
-        // 2. format the phone number like this 123-4567 or this 123-456-7890
+	
+	$n = strpos($name, ' ');
+        if ($n === false) {
+            $first_name = $name;
+        } else {
+	      $first_name = substr($name, 0, $n);
+        }
 
-        /*************************************************
-         * Display the validation message
-         ************************************************/
-        $message = "This page is under construction.\n" .
-                   "Please write the code that process the data.";
+        // make sure the user enters a valid email
 
-        break;
+	if (empty($email)) {
+	   $message = 'Email cannot be blank.';
+           break;
+        } else if(strpos($email, '@') === false) {
+           $message = 'Email must include an @ sign.';
+           break;
+        } else if(strpos($email, '.') === false) {
+           $message = 'The email must contain a period.';
+           break;
+        }
+
+        // Check phone number
+        $phone = str_replace('-', '', $phone);
+        $phone = str_replace('(', '', $phone);
+        $phone = str_replace(')', '', $phone);
+        $phone = str_replace(' ', '', $phone);
+
+        // validate the phone number
+        if (strlen($phone) < 7) {
+            $message = 'The phone number must contain at least seven digits.';
+            break;
+        }
+
+        // format the phone number
+        if (strlen($phone) == 7) {
+            $part1 = substr($phone, 0, 3);
+            $part2 = substr($phone, 3);
+            $phone = $part1 . '-' . $part2;
+        } else {
+            $part1 = substr($phone, 0, 3);
+            $part2 = substr($phone, 3, 3);
+            $part3 = substr($phone, 6);
+            $phone = $part1 . '-' . $part2 . '-' . $part3;
+        }
+
+	
+
+        $message =
+            "Hello $first_name,\n\n" .
+            "Thank you for entering this data:\n\n" .
+            "Name: $name\n" .
+            "Email: $email\n" .
+            "Phone: $phone\n";
+
+	break;
+
 }
 include 'string_tester.php';
 ?>
